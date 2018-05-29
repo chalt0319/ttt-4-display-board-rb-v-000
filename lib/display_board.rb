a29ba84ad14da56ea18dae7700fb709f028a42cd
chalt0319@gmail.com <chalt0319@gmail.com>
2018-05-29 20:04:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -68,7 +68,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["X"," "," "," ","X"," "," "," ","X"]
 display_board(board)
 
 def display_board(board)
