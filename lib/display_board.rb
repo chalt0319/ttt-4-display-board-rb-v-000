bcb69066e7b9af4d59ccb86633c37a37651c24f1
chalt0319@gmail.com <chalt0319@gmail.com>
2018-05-29 20:05:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -98,15 +98,5 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 display_board(board)
